7938bd0b0dd16edaa0c96fd21d5eb84cca88a551
Jaishri Bhagat <bhagat03jaishri@gmail.com>
2025-03-14 23:18:44 -0700

    Deploy updated build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  standalone: true,
+  imports: [CommonModule]
 })
 export class AppComponent {
   title = 'my-portfolio';
